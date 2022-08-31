1a217de6a95bd8ac0818e936c783c7d454a02bc9
FlutLab.io <>
2022-08-31 23:31:33 +0000

    displayed initial list of teams

---
+++ lib/ui/lista_equipes.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ListaEquipes extends StatelessWidget {
+  ListaEquipes({Key? key}) : super(key: key);
+
+  final equipes = [
+    {
+      "nome": "Catar",
+      "grupo": "A",
+      "url_simbolo":
+          "https://upload.wikimedia.org/wikipedia/pt/a/a9/Associa%C3%A7%C3%A3o_do_Qatar_de_Futebol.png"
+    },
+    {
+      "nome": "Equador",
+      "grupo": "A",
+      "url_simbolo": "https://upload.wikimedia.org/wikipedia/pt/7/74/FEFecu.png"
+    },
+    {
+      "nome": "Inglaterra",
+      "grupo": "B",
+      "url_simbolo":
+          "https://upload.wikimedia.org/wikipedia/pt/2/2e/England_crest_2009.svg.png"
+    },
+    {
+      "nome": "Países Baixos",
+      "grupo": "A",
+      "url_simbolo":
+          "https://upload.wikimedia.org/wikipedia/pt/7/7c/FSenegalaiseF.png"
+    },
+    {
+      "nome": "Senegal",
+      "grupo": "A",
+      "url_simbolo":
+          "https://upload.wikimedia.org/wikipedia/pt/7/7c/FSenegalaiseF.png"
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: equipes.length,
+        itemBuilder: (context, i) {
+          var equipe = equipes[i];
+          var simbolo = Image.network(equipe["url_simbolo"]!);
+          return ListTile(
+              leading: simbolo,
+              title: Text(equipe["nome"]!),
+              subtitle: Text("Grupo " + equipe["grupo"]!));
+        });
+  }
+}
